dd7891ed4c3a1fa885ffc3b6ae362b67eb607326
Ryan Seit <ryan@ryanseit.com>
2020-02-10 16:16:36 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -44,9 +44,9 @@ def total_even_pairs(src)
   total = Array.new
   while row_index < src.count do
     if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
-      total += (src[row_index][0] + src[row_index][1])
+      total += src[row_index][0] + src[row_index][1]
     end
     row_index +=1
   end
-  total.to_i
+  puts total.to_i
 end
